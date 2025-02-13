9d2ba403ac51bfe23b263a4ff9e4539da38185db
Dharansh Neema <dharanshneema@gmail.com>
2025-02-13 22:35:07 +0530

    PORT setup done

---
+++ app.js
@@ -0,0 +1,26 @@
+// Express
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+//Regulare middleware
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// Morgan logger
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+PORT = process.env.PORT || 8030;
+app.listen(PORT, () => {
+  console.log(`Server is running at: ${PORT}`);
+});
